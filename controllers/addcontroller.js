d989fbc3398878d986dc394ea0dc44c434b1e986
Xian Kai Ng <kingxkok@gmail.com>
2018-09-03 01:14:32 -0700

    moved texting to add

--- controllers/addcontroller.js
+++ controllers/addcontroller.js
@@ -2,6 +2,32 @@ const { MongoClient, ObjectId } = require('mongodb');
 const util = require('./utilController');
 const debug = require('debug')('app:addController');
 
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-west-2'});
+
+function generateMessage({topic, time, date, location, replyTo})  {
+    return `Reminder for appointment with World Relief\n\
+Topic: ${topic}\n\
+${time} ${date}\n\
+${location}\n\
+For Replies: ${replyTo}`;
+}
+
+const sendSMS = ({topic, time, date, location, replyTo, phoneNumber}) => {
+    let params = {
+        Message: generateMessage({topic, time, date, location, replyTo}),
+        PhoneNumber: phoneNumber,
+    };
+
+    var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+    publishTextPromise.then(
+        function(data) {
+            console.log("MessageID is " + data.MessageId);
+        }).catch( function(err) {
+            console.error(err, err.stack);
+    });
+}
+
 exports.addTask = (req, res) => {
   res.render('addTask', { title: 'Adding a Task' });
 };
@@ -12,6 +38,12 @@ exports.saveTask = async (req, res) => {
     const dbParams = await util.setupDB();
     await dbParams.collection.insertOne(task);
     dbParams.client.close();
+
+    console.log(req.body);
+    // sendSMS({topic: "N-400", time: "11.05", date: "Sep 30, 2018", 
+    //   location: "13121 Brookhurst St H, Garden Grove",
+    //   replyTo: "714-210-4730", phoneNumber: '+18585393586'});
+
     res.redirect('/');
   }
 
--- controllers/completeController.js
+++ controllers/completeController.js
@@ -2,31 +2,7 @@ const { MongoClient, ObjectId } = require('mongodb');
 const util = require('./utilController');
 const debug = require('debug')('app:completeController');
 
-var AWS = require('aws-sdk');
-AWS.config.update({region: 'us-west-2'});
 
-function generateMessage({topic, time, date, location, replyTo})  {
-    return `Reminder for appointment with World Relief\n\
-Topic: ${topic}\n\
-${time} ${date}\n\
-${location}\n\
-For Replies: ${replyTo}`;
-}
-
-const sendSMS = ({topic, time, date, location, replyTo, phoneNumber}) => {
-    let params = {
-        Message: generateMessage({topic, time, date, location, replyTo}),
-        PhoneNumber: phoneNumber,
-    };
-
-    var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
-    publishTextPromise.then(
-        function(data) {
-            console.log("MessageID is " + data.MessageId);
-        }).catch( function(err) {
-            console.error(err, err.stack);
-    });
-}
 
 exports.commitComplete = async (req, res) => {
     try {
@@ -36,11 +12,7 @@ exports.commitComplete = async (req, res) => {
         let status = (task.isComplete == 'false') ? 'true' : 'false';
         await dbParams.collection.findOneAndUpdate({ _id: new ObjectId(id) }, { $set: { isComplete: status } });
         dbParams.client.close();
-
-        console.log('SMS about to send...');
-        sendSMS({topic: "N-400", time: "11.05", date: "Sep 30, 2018", 
-            location: "13121 Brookhurst St H, Garden Grove",
-            replyTo: "714-210-4730", phoneNumber: '+18585393586'});
+        
         res.redirect('/');
     }
 
